c1a31c21c2423bddf6c941cf125020894f13caa6
Liron <liron.moryosef@pixoneye.com>
2018-08-28 08:32:18 +0300

    Code after starting redux and part 44 of videos

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// creating the reducer:
+export default function() {
+    return [
+        { title: 'javascript the good parts' },
+        { title: 'Harry Potter' },
+        { title: 'The dark tower 1' },
+        { title: 'The dark tower 2' }
+    ];
+} 
